990b683627ec6ab8af01ed4d6b38ca9fdf1b9677
Jonatan Hedborg jonatan.hedborg@gmail.com <Jonatan Hedborg jonatan.hedborg@gmail.com>
2017-07-05 14:22:34 +0200

    Initial commit

---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+	public Rigidbody BodyToAffect;
+
+	public float DownwardsThrust = 25.0f;
+	public float PerpendicularThrust = 10.0f;
+	public bool TurnsClockwise = false;
+	public float Power = 0.0f;
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		Power = Mathf.Clamp(Power, 0, 1);
+		Vector3 upForceVector = new Vector3(0f, DownwardsThrust * Power, 0f);
+		Vector3 perpForceVector = new Vector3(TurnsClockwise ? PerpendicularThrust * Power: -PerpendicularThrust * Power, 0f, 0f);
+		BodyToAffect.AddForceAtPosition(transform.rotation * upForceVector, transform.position);
+		BodyToAffect.AddForceAtPosition(transform.rotation * perpForceVector, transform.position);
+	}
+}
---
+++ Assets/Scripts/QuadController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadController : MonoBehaviour {
+	public IMUSensor Sensor;
+	public Motor FrontLeftMotor, FrontRightMotor, BackLeftMotor, BackRightMotor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FrontRightMotor.Power = 0f;
+		FrontLeftMotor.Power = 0f;
+		BackRightMotor.Power = 0f;
+		BackLeftMotor.Power = 0f;
+
+		if (Input.GetButton("F_R"))
+		{
+			FrontRightMotor.Power = 1f;
+		}
+
+		if (Input.GetButton("F_L"))
+		{
+			FrontLeftMotor.Power = 1f;
+		}
+
+		if (Input.GetButton("B_R"))
+		{
+			BackRightMotor.Power = 1f;
+		}
+
+		if (Input.GetButton("B_L"))
+		{
+			BackLeftMotor.Power = 1f;
+		}
+
+	}
+}
